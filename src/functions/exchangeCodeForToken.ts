c6ec3c95975db9acf82f494235f4f8696cbf2a7c
Ilya Shestakov <siavol@gmail.com>
2024-08-22 23:23:44 +0300

    function to exchange github code

--- src/functions/exchangeCodeForToken.ts
+++ src/functions/exchangeCodeForToken.ts
@@ -3,9 +3,37 @@ import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/fu
 export async function exchangeCodeForToken(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     context.log(`Http function processed request for url "${request.url}"`);
 
-    const name = request.query.get('name') || await request.text() || 'world';
+    const code = request.query.get('code');
 
-    return { body: `Hello, ${name}!` };
+    const clientId = process.env.GITHUB_APP_CLIENT_ID
+    const clientSecret = process.env.GITHUB_APP_CLIENT_SECRET
+    const url = `https://github.com/login/oauth/access_token?client_id=${clientId}&client_secret=${clientSecret}&code=${code}`
+    console.log('POST ' + url)
+    const response = await fetch(url, {
+      method: 'POST',
+    })
+    
+    if (!response.ok) {
+        const details = await response.json()
+        return {
+            status: 500,
+            body: JSON.stringify({
+                error: 'GitHub request failed'
+            })
+        }
+    }
+      
+    const resText = await response.text();
+    const tokenData = resText
+        .split('&')
+        .map(i => i.split('=', 2))
+        .reduce((acc, [key, val]) => {
+            acc[key]=val; 
+            return acc
+        }, {})
+  
+
+    return { body: JSON.stringify(tokenData) };
 };
 
 app.http('exchangeCodeForToken', {
